b45b69a21a3aeadab227254034ca7c22b179bcb3
Arun Vaishnav <arun.vaishnav@green-apex.com>
2020-12-23 13:51:53 +0530

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  get '/typehead/:query', to: 'users#typehead'
+  namespace :api do
+  	resources :users
+  	get '/typehead/:query', to: 'users#typehead'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
